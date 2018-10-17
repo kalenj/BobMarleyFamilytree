2a8b86b30241ae7e4d2daadfcce085a12946af62
kalen jackson <kalen.jackson@hgs.hiddengeniusproject.org>
2018-10-16 18:01:01 -0700

    Bobmarley tree

---
+++ Marleyfamilytree.sh
@@ -0,0 +1,22 @@
+cd ~/Desktop
+mkdir BobMarley
+cd BobMarley
+mkdir Cedella Ziggy KY-Mani
+cd Cedella
+mkdir Skip 
+cd Skip
+mkdir sm_songs
+cd sm_songs
+touch lions.reggae lifeboat.reggae
+cd ../../../Ziggy/
+touch daughter_1.child daughter_2.child
+mkdir zm_songs
+cd zm_songs
+touch drive.reggae a_lifetime.reggae 
+cd ../../KY-Mani/
+mkdir km_songs movies
+cd km_songs 
+touch dear_dad.reggae mama.reggae
+cd ../movies/
+touch shottaz.action one_love.drama
+cd ~/Desktop
